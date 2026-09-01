f544625f4c93d648e44dfc68632f35a6581e1620
Lucas <lucastimmprocopio@gmail.com>
2026-09-01 14:52:20 -0300

    Injeta acoplamento ciclico model <-> vet; remove pasta .sonargraph do build

--- petclinic_injetada/src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
+++ petclinic_injetada/src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
@@ -1,38 +1,26 @@
-/*
- * Copyright 2012-2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.model;
+
+import org.springframework.samples.petclinic.vet.Specialty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.validation.constraints.NotBlank;
 
 /**
  * Simple JavaBean domain object adds a name property to <code>BaseEntity</code>. Used as
  * a base class for objects needing these properties.
- *
- * @author Ken Krebs
- * @author Juergen Hoeller
- * @author Wick Dynex
  */
 @MappedSuperclass
 public class NamedEntity extends BaseEntity {
 
-	@Column
-	@NotBlank
+	@Column(name = "name")
 	private String name;
+
+	// --- ACOPLAMENTO CICLICO INTENCIONAL (demonstracao do seminario) ---
+	// model.NamedEntity passa a depender de vet.Specialty; como vet ja
+	// depende de model, fecha-se o ciclo model <-> vet (viola o ADP).
+	@SuppressWarnings("unused")
+	private transient Specialty demoSpecialty;
+	// -------------------------------------------------------------------
 
 	public String getName() {
 		return this.name;
@@ -44,8 +32,7 @@ public class NamedEntity extends BaseEntity {
 
 	@Override
 	public String toString() {
-		String name = this.getName();
-		return name != null ? name : "<null>";
+		return this.getName();
 	}
 
 }
